cffa5ee8de8a4c1a10140bb338335b81510845fe
Ava Randa <avaranda@umich.edu>
2017-02-05 23:22:33 -0500

    updated homework with potential solution to 2b

--- 206W17_HW3.py
+++ 206W17_HW3.py
@@ -4,7 +4,7 @@ import re
 ## SI 206 - W17 - HW3
 ## COMMENT WITH:
 ## Your section day/time:Sec 003, Thursdays, 6pm-7pm
-## Any names of people you worked with on this assignment: Danielle, 
+## Any names of people you worked with on this assignment: Danielle, Sadie, Samii
 
 #####################
 
@@ -39,16 +39,27 @@ print(parse_counted_words(test3))
 ## PART 2: 200 points
 
 ## We have provided a text file computer_paths.txt. It's not incredibly long -- you can scan through it, but do NOT hard code your answers! Each line contains 1 filesystem path.
-f = open('./computer_paths.txt', 'r')
-lines = f.read()
+#f = open('./computer_paths.txt', 'r')
+#lines = f.read()
 ## (a) Write Python code to determine how many of these paths identify FILES, not directories. Save that number in the variable file_paths_num.
-line = re.findall("\.\w+", lines)
-file_paths_num = len(line)
+#line = re.findall("\.\w+", lines)
+#file_paths_num = len(line)
 ## (b) Write Python code to determine how many of these paths are FULL paths, not relative paths. Save that number in the variable full_paths_num.
 
 ## (c) Write Python code to determine how many of these paths describe a Python file saved inside a folder called SI206. Save that number in the variable python_course_paths.
 
 ## (d) Write Python code to determine how many of these paths describe a Microsoft file (a file that EITHER ends with .docx OR .xlsx, but nothing else counts) where the file name ends in a digit. Save that total in the variable microsoft_files_num.
+f = open('./computer_paths.txt', 'r')
+contents = f.read()
+
+files = re.findall('\.\w+', contents)
+file_paths_num = len(files)
+
+fullpaths = re.findall('~\/|\/Users', contents)
+full_paths_num = len(fullpaths)
+
+
+
 
 
 
