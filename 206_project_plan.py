d75ccc4cfac99e60a63b9768f094f8657e41dce3
Ava Randa <avaranda@umich.edu>
2017-04-10 00:03:32 -0400

    second commit

--- 206_project_plan.py
+++ 206_project_plan.py
@@ -9,9 +9,20 @@ import requests
 import tweepy
 import twitter_info # Requires you to have a twitter_info file in this directory
 import re
-
 #######
-CACHE_FNAME = "SI206_finalproject_cache.json"
+## Tweepy authentication setup
+## Fill these in in the twitter_info.py file
+consumer_key = twitter_info.consumer_key
+consumer_secret = twitter_info.consumer_secret
+access_token = twitter_info.access_token
+access_token_secret = twitter_info.access_token_secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Set up to be able grab stuff from twitter with your authentication using Tweepy methods, and return it in a JSON format 
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+#######
+CACHE_FNAME = "SI206_final_project_cache.json"
 try:
 	cache_data = open(CACHE_FNAME, 'r')
 	cache_contents = cache_data.read()
@@ -53,7 +64,7 @@ class MyUnitTest(unittest.TestCase):
 	def test_7_movie_instance(self):
 		self.assertTrue(type(self.year_released) == type("a"))
 	def test_8_caching(self):
-		filename = open("SI206_finalproject_cache.json", 'r').read()
+		filename = open("SI206_final_project_cache.json", 'r').read()
 		self.assertTrue('Director' in filename)
 
 
